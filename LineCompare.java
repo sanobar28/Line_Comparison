b048e6c18730e07e316619a8c8678feac9e1a1fc
sanobar28 <sanobar.mujawar@gmail.com>
2021-06-06 20:04:29 +0530

    Welcome

---
+++ LineCompare.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @author Sanobar Mujawar
+ * @Created_on: 06.06.21
+ * 
+ * purpose: Line comparison program to compare length of two lines with co-ordinates. 
+ * 
+ * 
+ */
+
+package com;
+
+public class LineCompare {
+
+	public static void main(String[] args) {
+		
+		 System.out.println("Welcome to line comparison computation");
+		
+	}
+}
